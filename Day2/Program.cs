3785a4e8de0ad3110099c97eac64d43f3a5bde63
KingoBoiii <kingodk@outlook.dk>
2024-12-20 06:03:25 +0100

    Solved Part 1 of day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,8 @@
+﻿using AdventofCode2024.Day2;
+
+var safeReports = Part1.GetSafeReports("input.txt").ToArray();
+
+Console.WriteLine($"[Part1] Total safe reports: {safeReports.Length}");
+
+Console.Read();
+
